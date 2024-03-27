641cd1d225584883032bc27287f7e8b5aa9fa548
mrtzfr <mertcanzafer06@gmail.com>
2024-03-27 10:49:08 +0300

    Nicely updated!!!!!

--- PythonFiles/HistEqEx1.py
+++ PythonFiles/HistEqEx1.py
@@ -1,16 +1,61 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
+# Open the original image and convert it to grayscale
 original = Image.open("PythonFiles/tiger.jpg")
-original.show()
 original = original.convert('L')
-
+original.show()
+# Get the pixel values of the grayscale image
 PixelVals = list(original.getdata())
 
+# Get the dimensions (width and height) of the image
 width, height = original.size
 
+# Calculate the total number of pixels
 totalPixels = width * height
 
+# Calculate the histogram of the original image
+h = original.histogram()
+
+# Calculate the equalization limits
+slow = 0.40
+shigh = 1 - slow
+aMin = 0
+aMax = 255
+
+# Calculate alow and ahigh
+cumulative_sum = 0
+alow = aMin
+ahigh = aMax
+for idx, freq in enumerate(h):
+    cumulative_sum += freq
+    if cumulative_sum >= totalPixels * slow:
+        alow = idx
+        break
+
+cumulative_sum = 0
+for idx, freq in enumerate(h[::-1]):
+    cumulative_sum += freq
+    if cumulative_sum >= totalPixels * slow:
+        ahigh = 255 - idx
+        break
+
+# Apply manual histogram equalization
+equalized_pixels = []
+for pixel_value in PixelVals:
+    if pixel_value < alow:
+        equalized_pixels.append(aMin)
+    elif pixel_value > ahigh:
+        equalized_pixels.append(aMax)
+    else:
+        equalized_pixels.append(
+            int(((pixel_value - alow) / (ahigh - alow)) * (aMax - aMin)) + aMin)
+
+# Create a new image with equalized pixel values
+equalized_image = Image.new('L', original.size)
+equalized_image.putdata(equalized_pixels)
+
+# Display original and equalized histograms
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 plt.hist(PixelVals, bins=256, color='black', alpha=0.7)
@@ -18,77 +63,14 @@ plt.title('Histogram of Original Image')
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
 
-# Perform histogram equalization
-equalized_pixels = list(original.histogram())
-
-# Calculate and plot histogram for the equalized image
 plt.subplot(1, 2, 2)
 plt.hist(equalized_pixels, bins=256, color='black', alpha=0.7)
 plt.title('Histogram of Equalized Image')
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
 
-# Show the histograms
 plt.tight_layout()
 plt.show()
 
-h = list(original.histogram())
-
-aMin = 0
-aMax = 255
-alow = 0
-ahigh = 0
-slow = 0.05
-shigh = 1 - slow
-
-for i in h:
-    if (i >= totalPixels * slow):
-        alow = min(i)
-    else:
-        ahigh = max(i)
-
-for a in h:
-    if (a > alow and a < ahigh):
-        a = (((a - alow) / (ahigh - alow)) * (aMax - aMin)) + aMin
-    elif (alow >= a):
-        a = aMin
-    elif (a >= ahigh):
-        a = aMax
-
-# aMin = 0
-# aMax = 255
-# alow = 0
-# ahigh = 0
-# slow = 0.05
-# shigh = 1 - slow
-
-# for i in equalized_pixels:
-#     if (i >= totalPixels * slow):
-#         alow = min(i)
-#     else:
-#         ahigh = max(i)
-
-# for a in equalized_pixels:
-#     if (a > alow and a < ahigh):
-#         a = (((a - alow) / (ahigh - alow)) * (aMax - aMin)) + aMin
-#     elif (alow >= a):
-#         a = aMin
-#     elif (a >= ahigh):
-#         a = aMax
-
-# plt.figure(figsize=(12, 6))
-# plt.subplot(1, 2, 1)
-# plt.hist(PixelVals, bins=256, color='black', alpha=0.7)
-# plt.title('Histogram of Original Image')
-# plt.xlabel('Pixel Value')
-# plt.ylabel('Frequency')
-
-# plt.subplot(1, 2, 2)
-# plt.hist(equalized_pixels, bins=256, color='black', alpha=0.7)
-# plt.title('Histogram of Equalized Image')
-# plt.xlabel('Pixel Value')
-# plt.ylabel('Frequency')
-
-# # Show the histograms
-# plt.tight_layout()
-# plt.show()
+# Show the equalized image
+equalized_image.show()
